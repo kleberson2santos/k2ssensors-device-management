e3fe098bc8ab79dd0d13baf9979239a87f327148
Kleberson Santos <kleberson2santos@gmail.com>
2025-05-06 21:02:38 -0400

    Implements microservices

--- src/main/java/com/k2sworks/k2ssensors/device/management/api/controller/SensorController.java
+++ src/main/java/com/k2sworks/k2ssensors/device/management/api/controller/SensorController.java
@@ -1,20 +1,88 @@
 package com.k2sworks.k2ssensors.device.management.api.controller;
 
 import com.k2sworks.k2ssensors.device.management.api.model.SensorInput;
+import com.k2sworks.k2ssensors.device.management.api.model.SensorOutput;
 import com.k2sworks.k2ssensors.device.management.common.IdGenerator;
 import com.k2sworks.k2ssensors.device.management.domain.model.Sensor;
+import com.k2sworks.k2ssensors.device.management.domain.model.SensorId;
+import com.k2sworks.k2ssensors.device.management.domain.repository.SensorRepository;
+import io.hypersistence.tsid.TSID;
+import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.web.PageableDefault;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.server.ResponseStatusException;
 
 @RestController
 @RequestMapping("/api/sensors")
+@RequiredArgsConstructor
 public class SensorController {
+
+    private final SensorRepository sensorRepository;
+
+    @GetMapping
+    public Page<SensorOutput> search(@PageableDefault Pageable pageable) {
+        Page<Sensor> sensors = sensorRepository.findAll(pageable);
+        return sensors.map(this::convertToModel);
+    }
+
+    @GetMapping("{sensorId}")
+    public SensorOutput getOne(@PathVariable TSID sensorId) {
+        Sensor sensor = sensorRepository.findById(new SensorId(sensorId))
+                .orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND));
+        return convertToModel(sensor);
+    }
 
     @PostMapping
     @ResponseStatus(HttpStatus.CREATED)
-    public Sensor create(@RequestBody SensorInput input) {
+    public SensorOutput create(@RequestBody SensorInput input) {
+        Sensor sensor = toDomainModel(input);
+        sensor = sensorRepository.saveAndFlush(sensor);
+        return convertToModel(sensor);
+    }
+
+    @PutMapping("/{sensorId}")
+    public SensorOutput update(@PathVariable TSID sensorId,
+                               @RequestBody SensorInput input) {
+        Sensor sensor = sensorRepository.findById(new SensorId(sensorId))
+                .orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND));
+
+        sensor.setName(input.getName());
+        sensor.setLocation(input.getLocation());
+        sensor.setIp(input.getIp());
+        sensor.setModel(input.getModel());
+        sensor.setProtocol(input.getProtocol());
+
+        sensor = sensorRepository.save(sensor);
+
+        return convertToModel(sensor);
+    }
+
+    @DeleteMapping("/{sensorId}")
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    public void delete(@PathVariable TSID sensorId) {
+        Sensor sensor = sensorRepository.findById(new SensorId(sensorId))
+                .orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND));
+        sensorRepository.delete(sensor);
+    }
+
+    private SensorOutput convertToModel(Sensor model) {
+        return SensorOutput.builder()
+                .id(model.getId().getValue())
+                .name(model.getName())
+                .ip(model.getIp())
+                .location(model.getLocation())
+                .protocol(model.getProtocol())
+                .model(model.getModel())
+                .enabled(model.getEnabled())
+                .build();
+    }
+
+    private Sensor toDomainModel(SensorInput input) {
         return Sensor.builder()
-                .id(IdGenerator.generateTSID())
+                .id(new SensorId(IdGenerator.generateTSID()))
                 .name(input.getName())
                 .ip(input.getIp())
                 .location(input.getLocation())
